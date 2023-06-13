f66ccb1bb5f05435327b264cbe181ce7f2e8eb00
Soumodeep Maity <87257845+cse18107@users.noreply.github.com>
2023-06-13 17:20:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const app = express();
 
+const port = 3003
+
 app.get('/',(req,res) => {
     res.status(200).json({
         message:"Server Is Working."
     });
 })
 
-app.listen(3003, ()=> {
-    console.log('server is working on port 3003');
+app.listen(port, ()=> {
+    console.log(`server is working on port ${port}`);
 })
