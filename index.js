76c81e1c0bb6454aa76a359a052f6ca8dcdc04b3
Soumodeep Maity <87257845+cse18107@users.noreply.github.com>
2023-06-13 17:02:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.get('/hello',(req,res) => {
+app.get('/',(req,res) => {
     res.status(200).json({
-        message:"Working."
+        message:"Server Is Working."
     });
 })
 
